69807eec00c1bbb5a73211032884c364f0c8dcae
StangMKxD <phusa13fu@gmail.com>
2025-08-09 22:37:07 +0700

    first commit client

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ const ContactPage = () => {
   return (
     <>
       <div className="text-center">
-        <h1 className="text-9xl">ยังไม่ได้ทำรอแปปป</h1>
+        <h1 className="text-9xl">ยังไม่ได้ทำรอแปปปxD</h1>
         
       </div>
     </>
