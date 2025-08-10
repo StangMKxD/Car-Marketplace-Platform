5ddf4ee4688c6ca7fe13d5baec5152230529929d
StangMKxD <phusa13fu@gmail.com>
2025-08-10 22:03:37 +0700

    First commit to ProjectSecret

--- src/utils/logout.ts
+++
@@ -1,8 +0,0 @@
-import { toast } from "react-toastify"
-
-export const logout = () => {
-  localStorage.removeItem("token")
-  localStorage.removeItem("role")
-  toast.info("คุณได้ออกจากระบบแล้ว")
-  window.location.href = "/login"
-}
